62cd2db5226c01a3c335298bbd184299cee68823
Nick K. <nkokkos@gmail.com>
2026-05-27 15:14:45 +0300

    Update things_board_telemetry_worker.ex

--- gas_sensor/lib/gas_sensor/things_board_telemetry_worker.ex
+++ gas_sensor/lib/gas_sensor/things_board_telemetry_worker.ex
@@ -2,18 +2,27 @@ defmodule GasSensor.TelemetryThingsboard do
   use GenServer
   require Logger
 
+  # What are we doing here:
+
+  # Every 15 seconds, we read from the ReadingAgent and we send out the information to the
+  # thingsboard api
+
+  # This is bad practice but anyway: I have hardcoded the telemetry period of 15 seconds = 15_000
 
   # use example from here
   # https://github.com/gausby/tortoise
   # https://www.codesync.global/uploads/media/activity_slides/0001/02/4f9231e35e2afeb0a669389afea70bec0be84863.pdf
   # https://www.codesync.global/media/tortoise-evolved-mqtt-5-support-in-tortoise-mqtt-client/
 
+  # implement these too in the future
   # try also these examples from here:
   # https://github.com/emqx/emqtt
   # https://www.emqx.com/en/blog/mqtt-for-elixir
 
+  # Need to include the nerves key for X.509 certificates. 
+  # Right now, this thing works with simple access token
+
   # Constants from your mosquitto_pub example
-  
   @client_id "gas_sensor_001"
   @host "mqtt.eu.thingsboard.cloud"
   @port 1883
@@ -47,12 +56,17 @@ defmodule GasSensor.TelemetryThingsboard do
 
   @impl true
   def handle_info(:send_telemetry, state) do
-    
-    #payload = Jason.encode!(%{voltage: 3.335, status: "active"})
 
-    #remove the i2c key from the agent since we do not need it. 
+    # example:
+    # payload = Jason.encode!(%{voltage: 3.335, status: "active"})
+
+    # read latest data from the ReadingAgent
     data_point = GasSensor.ReadingAgent.get_reading()
-    data_point_without_i2c = Map.delete(data_point, :i2c)    
+    
+    # remove the i2c key from the agent since we do not need it. PLus, 
+    # it breaks up json encoding
+    data_point_without_i2c = Map.delete(data_point, :i2c)
+
     payload = Jason.encode!(data_point_without_i2c)
 
     case Tortoise.publish(@client_id, @topic, payload, qos: 1) do
